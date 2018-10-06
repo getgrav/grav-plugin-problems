77eb75071acfdd8472cb31a6b5216f70d4edf132
Andy Miller <rhuk@mac.com>
2018-10-06 17:56:18 -0600

    Revert "requires 1.6"
    This reverts commit a89280af944bb9c839f8c163bb6c811f3a05a37b.

--- cli/CheckCommand.php
+++ cli/CheckCommand.php
@@ -50,7 +50,7 @@ class CheckCommand extends ConsoleCommand
         $io->title('Grav Problems');
 
         $table = new Table($this->output);
-        $table->setStyle('box');
+        $table->setStyle('default');
         $headers = ['ID', 'Status', 'Level', 'Message'];
         $rows = [];
 
