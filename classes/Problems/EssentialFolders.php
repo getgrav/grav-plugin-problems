a7782040d419efa9a63a5960d12a04d459d1a703
Markus Hansmair <mh_github@gmx.net>
2021-04-06 20:50:29 +0200

    adapted to configurable paths
    see https://github.com/getgrav/grav/issues/3297

--- classes/Problems/EssentialFolders.php
+++ classes/Problems/EssentialFolders.php
@@ -19,35 +19,37 @@ class EssentialFolders extends Problem
     public function process()
     {
         $essential_folders = [
-            'backup' => true,
-            'cache' => true,
-            'logs' => true,
-            'images' => true,
-            'assets' => true,
-            'system' => false,
-            'user/data' => true,
-            'user/pages' => false,
-            'user/config' => false,
-            'user/plugins/error' => false,
-            'user/plugins' => false,
-            'user/themes' => false,
-            'vendor' => false,
-            'tmp' => true,
+            (str_starts_with(GRAV_BACKUP_PATH, '/') ? '' : ROOT_DIR) . GRAV_BACKUP_PATH => true,
+            (str_starts_with(GRAV_CACHE_PATH, '/') ? '' : ROOT_DIR) . GRAV_CACHE_PATH => true,
+            (str_starts_with(GRAV_LOG_PATH, '/') ? '' : ROOT_DIR) . GRAV_LOG_PATH => true,
+            GRAV_WEBROOT . '/images' => true,
+            GRAV_WEBROOT . '/assets' => true,
+            (str_starts_with(GRAV_SYSTEM_PATH, '/') ? '' : ROOT_DIR) . GRAV_SYSTEM_PATH => false,
+            USER_DIR . 'data' => true,
+            USER_DIR . 'pages' => false,
+            USER_DIR . 'config' => false,
+            USER_DIR . 'plugins/error' => false,
+            USER_DIR . 'plugins' => false,
+            USER_DIR . 'themes' => false,
+            ROOT_DIR . 'vendor' => false,
+            (str_starts_with(GRAV_TMP_PATH, '/') ? '' : ROOT_DIR) . GRAV_TMP_PATH => false,
         ];
 
         // Check for essential files & perms
         $file_errors = [];
         $file_success = [];
 
-        foreach ($essential_folders as $file => $check_writable) {
-            $file_path = ROOT_DIR . $file;
-
+        foreach ($essential_folders as $file_path => $check_writable) {
             if (!file_exists($file_path)) {
                 $file_errors[$file_path] = 'does not exist';
-            } elseif ($check_writable && !is_writable($file_path)) {
-                $file_errors[$file_path] = 'exists but is <strong>not writeable</strong>';
+            } elseif (!$check_writable) {
+                $file_success[$file_path] = 'exists';
             } else {
-                $file_success[$file_path] = 'exists and is writable';
+                if (is_writable($file_path)) {
+                    $file_success[$file_path] = 'exists and is writable';
+                } else {
+                    $file_errors[$file_path] = 'exists but is <strong>not writeable</strong>';
+                }
             }
         }
 
